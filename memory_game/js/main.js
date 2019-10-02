2c7d55ffc0efa2a5a51302ae12987f49d10584f8
Marissa Epstein <mar@roma-lenovo-pc.home>
2019-10-02 14:01:35 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,31 +24,37 @@ var cards = [
 
 
 var cardsInPlay = [];
-
-function checkForMatch() {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
-	} else {
-		console.log("Sorry, try again.");
+function createBoard () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);  
+		cardElement.addEventListener('click', flipCard); 
+		document.getElementById('game-board').appendChild(cardElement);
 	}
 }
 
-function flipCard(cardId) {
+
+function checkForMatch() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+function flipCard() {
+	var cardId = this.getAttribute('data-id'); 
 	console.log("User flipped " + cards[cardId].rank); 	
+	console.log("User flipped " + cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank); 
 	console.log(cards[cardId].cardImage); 
-	console.log(cards[cardId].suit);
-}
-
-if (cardsInPlay.length === 2){
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2){
 	checkForMatch();
+	}
 }
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
-
-
-
+createBoard();
 
 
